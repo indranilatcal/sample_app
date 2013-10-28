27685763f6208ea777aff567d5fe0c0a32bee0e8
Indranil Chatterjee <indranilatcal@gmail.com>
2013-10-28 22:53:07 +0530

    Cleaned up tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,11 +29,28 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
+
+			describe "after submission" do
+
+				before { click_button submit }
+
+				it { should have_content('Sign up') }
+				it { should have_content('errors') }
+				it { should have_content("Name can't be blank") }
+				it { should have_content("Email can't be blank") }
+				it { should have_content("Email is invalid") }
+				it { should have_content("Password can't be blank") }
+				it { should have_content("Password is too short (minimum is 6 characters)") }
+			end
 		end
 
 		describe "with valid information" do
+
+			let(:name) { "Example User" }
+			let(:email) { "user@example.com" }
+
 			before do
-				fill_in "Name",         with: "Example User"
+				fill_in "Name",         with: name
         		fill_in "Email",        with: "user@example.com"
         		fill_in "Password",     with: "foobar"
         		fill_in "Confirmation", with: "foobar"
@@ -41,6 +58,16 @@ describe "User pages" do
 
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
+			end
+
+			describe "after saving the user" do
+
+				before { click_button submit }
+				let(:user) { User.find_by(email: email) }
+
+				it { should have_title(user.name) }
+				it { should have_content("Welcome to the Sample App!") }
+				it { should have_selector('div.alert.alert-success', text: "Welcome") }
 			end
 		end
 	end
